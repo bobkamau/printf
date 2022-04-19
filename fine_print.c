82387b9c9c12e5b05c56c6ea271dd3a02cb41f33
clementndiritu <clemndiritu@gmail.com>
2022-04-20 01:06:33 +0300

    fine_print.c

--- fine_print.c
+++ fine_print.c
@@ -4,7 +4,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output,
 		unsigned long int num, char *base,
-		unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
 
 /**
  * convert_sbase - Converts a signed long to an inputted base and stores
@@ -21,7 +21,7 @@ unsigned int convert_ubase(buffer_t *output,
 
 
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-	unsigned char flags, int wid, int prec)
+		unsigned char flags, int wid, int prec)
 {
 	int size;
 	char digit, pad = '0';
@@ -55,7 +55,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 
 /**
  * convert_ubase - Converts an unsigned long to an inputted base and
- * stores the result to a buffer contained in a struct.
+ *                 stores the result to a buffer contained in a struct.
  * @output: A buffer_t struct containing a character array.
  * @num: An unsigned long to be converted.
  * @base: A pointer to a string containing the base to convert to.
@@ -79,6 +79,7 @@ unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 	if (num >= size)
 		ret += convert_ubase(output, num / size, base,
 				flags, wid - 1, prec - 1);
+
 	else
 	{
 		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
